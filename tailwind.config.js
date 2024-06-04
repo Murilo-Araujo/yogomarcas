907be6d3f48273d77b59062cca54727adbcea382
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2024-06-04 09:15:08 -0300

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,70 +4,77 @@ const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: [
-    './app/controllers/**/*.rb',
-    './app/models/**/*.rb',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/assets/stylesheets/**/*.{scss,sass,css}',
-    './app/views/**/*.{erb,haml,html,slim,jbuilder}',
-    './config/initializers/heroicon.rb',
-    './app/components/**/*.{rb,erb,haml,html,slim}',
-  //   locales i18n rails
-      './config/locales/**/*.{rb,yml}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans]
-      },
-      ringOffsetWidth: {
-        '2': '2px',
-        '4': '4px',
-        '8': '8px',
-      },
-      screens: {
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... }
+    content: [
+        './app/controllers/**/*.rb',
+        './app/models/**/*.rb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/assets/stylesheets/**/*.{scss,sass,css}',
+        './app/views/**/*.{erb,haml,html,slim,jbuilder}',
+        './config/initializers/heroicon.rb',
+        './app/components/**/*.{rb,erb,haml,html,slim}',
+        //   locales i18n rails
+        './config/locales/**/*.{rb,yml}',
+    ],
+    theme: {
+        container: {
+            padding: {
+                DEFAULT: '2rem'
 
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
+            },
+        },
 
-        'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
+        extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans]
+            },
+            ringOffsetWidth: {
+                '2': '2px',
+                '4': '4px',
+                '8': '8px',
+            },
+            screens: {
+                'sm': '640px',
+                // => @media (min-width: 640px) { ... }
 
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
+                'md': '768px',
+                // => @media (min-width: 768px) { ... }
 
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
+                'lg': '1024px',
+                // => @media (min-width: 1024px) { ... }
 
-      },
-      // Create your own at: https://javisperez.github.io/tailwindcolorshades
-      colors: {
-        primary: colors.green,
-        danger: colors.rose,
-        info: colors.blue,
-        warning: colors.amber,
-        success: colors.emerald,
-        secondary: colors.gray,
-        "code-400": "#fefcf9",
-        "code-600": "#3c455b",
-        "document-preview": "#E7E8EB"
-      },
-      margin: {
-        '1px': '1px',
-      }
+                'xl': '1280px',
+                // => @media (min-width: 1280px) { ... }
+
+                '2xl': '1536px',
+                // => @media (min-width: 1536px) { ... }
+
+            },
+            // Create your own at: https://javisperez.github.io/tailwindcolorshades
+            colors: {
+                primary: colors.green,
+                danger: colors.rose,
+                info: colors.blue,
+                warning: colors.amber,
+                success: colors.emerald,
+                secondary: colors.gray,
+                "code-400": "#fefcf9",
+                "code-600": "#3c455b",
+                "document-preview": "#E7E8EB"
+            },
+            margin: {
+                '1px': '1px',
+            }
+        },
     },
-  },
-  variants: {
-    ...defaultTheme.variants,
-    borderWidth: ['responsive', 'last', 'hover', 'focus']
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp')
-  ]
+    variants: {
+        ...defaultTheme.variants,
+        borderWidth: ['responsive', 'last', 'hover', 'focus']
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp')
+    ]
 }
